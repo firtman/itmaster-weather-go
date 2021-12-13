30e75de7338800dcd0c1e571ec173f98d00dc074
Maximiliano Firtman <firtman@gmail.com>
2021-12-13 12:18:40 -0300

    goroutines with waitgroup

--- main.go
+++ main.go
@@ -3,10 +3,26 @@ package main
 import (
 	"andreani/goweather/api"
 	"fmt"
+	"sync"
 )
 
-func main() {
-	wc, err := api.GetWeather("Madrid")
+func main() { // main routine
+	dummyCities := []string{"Madrid", "Buenos Aires", "Pilar"}
+	var wg sync.WaitGroup
+	for _, city := range dummyCities {
+		wg.Add(1)
+		go func(city string) {
+			renderWeather(city)
+			wg.Done()
+		}(city)
+	}
+	wg.Wait() // wait synchronously to the whole group
+	// time.Sleep(3 * time.Second)  Hack
+}
+
+func renderWeather(city string) {
+	fmt.Println("Fetching weather for ", city, " ⌛️")
+	wc, err := api.GetWeather(city)
 	if err != nil {
 		fmt.Println("We couldn't fetch the weather: ", err.Error())
 	} else {
